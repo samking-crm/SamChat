3fdc096084f4627fda0329e067b2d0393f437a84
Samking web development expert <samkingdigitalempire@gmail.com>
2026-03-24 08:42:53 +0100

    Create tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+// frontend/tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      animation: {
+        'bounce-slow': 'bounce 2s infinite',
+      }
+    },
+  },
+  plugins: [],
+}
