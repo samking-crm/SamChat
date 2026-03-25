8ed2665fc0ad52d8a5030040b02f34578a944ad5
Samking web development expert <samkingdigitalempire@gmail.com>
2026-03-25 10:22:30 +0100

    Create Post.js

---
+++ backend/server/src/models/Post.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  content: { type: String, trim: true },
+  image: { type: String },
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  comments: [{
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    content: String,
+    createdAt: { type: Date, default: Date.now }
+  }]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Post', postSchema);
