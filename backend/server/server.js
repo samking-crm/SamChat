b1ad0efee086bfb826ffceda73727b3dcf593402
Samking web development expert <samkingdigitalempire@gmail.com>
2026-03-29 18:16:01 +0100

    Update server.js

--- backend/server/server.js
+++ backend/server/server.js
@@ -2,3 +2,20 @@
 app.use('/api/users', require('./src/routes/users'));
 app.use('/api/messages', require('./src/routes/messages'));
 app.use('/api/posts', require('./src/routes/posts'));
+// ... existing code ...
+
+// Import socket handler
+const socketHandler = require('./sockets/socketHandler');
+
+// Socket.IO - FULL IMPLEMENTATION
+io.on('connection', (socket) => {
+  console.log('🔌 Socket connected:', socket.id);
+  
+  // Health check
+  socket.emit('connection_success', { message: 'Connected to SamChat!' });
+  
+  // Delegate to handler
+  socketHandler(io, socket);
+});
+
+// ... rest of server.js
